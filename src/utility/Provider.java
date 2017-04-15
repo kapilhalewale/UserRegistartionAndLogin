e4a9524d02663badfed7120ab66dbf2520b91f5f
Kapil Halewale <kapilhalewale@users.noreply.github.com>
2017-04-15 13:27:16 -0700

    Update Provider.java

--- src/utility/Provider.java
+++ src/utility/Provider.java
@@ -5,6 +5,6 @@ public interface  Provider
 {  
 	String DRIVER="com.mysql.jdbc.Driver";  
 	String CONNECTION_URL="jdbc:mysql://localhost/mypeople"; 
-	String USERNAME="root";  
-	String PASSWORD="database";  
+	String USERNAME="xxxxx";  
+	String PASSWORD="xxxxx";  
 		}  
